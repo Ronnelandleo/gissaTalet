f6d552181b87697a56fc9cbd737f2d7060f42bd1
Leo Ronneland <04LEROA@skola.lidkoping.se>
2021-11-08 15:40:07 +0100

    fixat en while loop

--- gissaTalet/Program.cs
+++ gissaTalet/Program.cs
@@ -9,135 +9,140 @@ namespace Uppgift_4._8
         {
 
             string anvandarensVal = "0";
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Välkommen till gissa talet");
-            Thread.Sleep(400);
-            Console.WriteLine("Välj ett alternativ!!!");
-            Thread.Sleep(400);
-            Console.WriteLine("1. Spela gissa talet 1-10");
-            Thread.Sleep(400);
-            Console.WriteLine("2. Spela gissa talet 1-100");
-            Thread.Sleep(400);
-            Console.WriteLine("3. Spela gissa talet 1-1000");
-            Thread.Sleep(400);
-            Console.WriteLine("4. Spelets regler");
-            Thread.Sleep(400);
-            Console.WriteLine("5. Avsluta programet");
-            anvandarensVal = Console.ReadLine();
-
-            Random random = new Random();
-
-            switch (anvandarensVal)
+            while (anvandarensVal != "5")
             {
-                //spela gissa talet 1-10
-                case "1":
-                    int datornsGissning = random.Next(1, 11);
-                    Console.WriteLine("Siffran skapas");
-                    Thread.Sleep(600);
-                    Console.WriteLine("gissa på en siffra mellan 1-10");
-                    int anvandarensGissning = int.Parse(Console.ReadLine());
 
 
-                    while (anvandarensGissning != datornsGissning)
-                    {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Välkommen till gissa talet");
+                Thread.Sleep(400);
+                Console.WriteLine("Välj ett alternativ!!!");
+                Thread.Sleep(400);
+                Console.WriteLine("1. Spela gissa talet 1-10");
+                Thread.Sleep(400);
+                Console.WriteLine("2. Spela gissa talet 1-100");
+                Thread.Sleep(400);
+                Console.WriteLine("3. Spela gissa talet 1-1000");
+                Thread.Sleep(400);
+                Console.WriteLine("4. Spelets regler");
+                Thread.Sleep(400);
+                Console.WriteLine("5. Avsluta programet");
+                anvandarensVal = Console.ReadLine();
+
+                Random random = new Random();
+
+                switch (anvandarensVal)
+                {
+                    //spela gissa talet 1-10
+                    case "1":
+                        int datornsGissning = random.Next(1, 11);
+                        Console.WriteLine("Siffran skapas");
+                        Thread.Sleep(600);
+                        Console.WriteLine("gissa på en siffra mellan 1-10");
+                        int anvandarensGissning = int.Parse(Console.ReadLine());
 
 
-                        if (anvandarensGissning > datornsGissning)
+                        while (anvandarensGissning != datornsGissning)
                         {
-                            Console.WriteLine("Lägre");
+
+
+                            if (anvandarensGissning > datornsGissning)
+                            {
+                                Console.WriteLine("Lägre");
+                            }
+                            else
+                            {
+                                Console.WriteLine("högre");
+                            }
+                            anvandarensGissning = int.Parse(Console.ReadLine());
                         }
-                        else
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("du gissade rätt!!!");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        break;
+
+
+
+                    //spela gissa talet 1-100
+                    case "2":
+                        int datornsGissning2 = random.Next(1, 101);
+                        Console.WriteLine("Siffran skapas");
+                        Thread.Sleep(600);
+
+                        Console.WriteLine("gissa på en siffra mellan 1-100");
+                        int anvandarensGissning2 = int.Parse(Console.ReadLine());
+
+
+                        while (anvandarensGissning2 != datornsGissning2)
                         {
-                            Console.WriteLine("högre");
+
+
+                            if (anvandarensGissning2 > datornsGissning2)
+                            {
+                                Console.WriteLine("Lägre");
+                            }
+                            else
+                            {
+                                Console.WriteLine("högre");
+                            }
+                            anvandarensGissning2 = int.Parse(Console.ReadLine());
                         }
-                        anvandarensGissning = int.Parse(Console.ReadLine());
-                    }
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("du gissade rätt!!!");
-                    Console.ForegroundColor = ConsoleColor.White;
 
-                    break;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("du gissade rätt!!!");
+                        Console.ForegroundColor = ConsoleColor.White;
 
+                        break;
 
 
-                //spela gissa talet 1-100
-                case "2":
-                    int datornsGissning2 = random.Next(1, 101);
-                    Console.WriteLine("Siffran skapas");
-                    Thread.Sleep(600);
+                    //spela gissa talet 1-1000
+                    case "3":
+                        int datornsGissning3 = random.Next(1, 1001);
+                        Console.WriteLine("Siffran skapas");
+                        Thread.Sleep(600);
 
-                    Console.WriteLine("gissa på en siffra mellan 1-100");
-                    int anvandarensGissning2 = int.Parse(Console.ReadLine());
-
-
-                    while (anvandarensGissning2 != datornsGissning2)
-                    {
+                        Console.WriteLine("gissa på en siffra mellan 1-1000");
+                        int anvandarensGissning3 = int.Parse(Console.ReadLine());
 
 
-                        if (anvandarensGissning2 > datornsGissning2)
+                        while (anvandarensGissning3 != datornsGissning3)
                         {
-                            Console.WriteLine("Lägre");
+
+
+                            if (anvandarensGissning3 > datornsGissning3)
+                            {
+                                Console.WriteLine("Lägre");
+                            }
+                            else
+                            {
+                                Console.WriteLine("högre");
+                            }
+                            anvandarensGissning3 = int.Parse(Console.ReadLine());
                         }
-                        else
-                        {
-                            Console.WriteLine("högre");
-                        }
-                        anvandarensGissning2 = int.Parse(Console.ReadLine());
-                    }
 
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("du gissade rätt!!!");
-                    Console.ForegroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("du gissade rätt!!!");
+                        Console.ForegroundColor = ConsoleColor.White;
 
-                    break;
+                        break;
 
 
-                //spela gissa talet 1-1000
-                case "3":
-                    int datornsGissning3 = random.Next(1, 1001);
-                    Console.WriteLine("Siffran skapas");
-                    Thread.Sleep(600);
+                    //spelets regler
+                    case "4":
+                        Console.WriteLine("I varje omgång av gissa talet kommer datorn slumpa fram ett heltal mellan 1-10, 1-100 eller 1-1000. Du ska gissa vilket talet är ända tills du gissar rätt. Efter varje gissning kommer du få veta om gissningar var rätt, för hög eller för låg.");
+                        break;
 
-                    Console.WriteLine("gissa på en siffra mellan 1-1000");
-                    int anvandarensGissning3 = int.Parse(Console.ReadLine());
-
-
-                    while (anvandarensGissning3 != datornsGissning3)
-                    {
+                    //avsluta programet
+                    case "5":
 
 
-                        if (anvandarensGissning3 > datornsGissning3)
-                        {
-                            Console.WriteLine("Lägre");
-                        }
-                        else
-                        {
-                            Console.WriteLine("högre");
-                        }
-                        anvandarensGissning3 = int.Parse(Console.ReadLine());
-                    }
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Du avslutade spelet");
+                        Console.ForegroundColor = ConsoleColor.White;
 
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("du gissade rätt!!!");
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                    break;
-
-
-                //spelets regler
-                case "4":
-                    Console.WriteLine("I varje omgång av gissa talet kommer datorn slumpa fram ett heltal mellan 1-10, 1-100 eller 1-1000. Du ska gissa vilket talet är ända tills du gissar rätt. Efter varje gissning kommer du få veta om gissningar var rätt, för hög eller för låg.");
-                    break;
-
-                //avsluta programet
-                case "5":
-
-
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Du avslutade spelet");
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                    break;
+                        break;
+                }
 
 
             }
